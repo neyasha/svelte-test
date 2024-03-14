589e69ebe34962a756b3cef38b8a4f4db8e68b21
nastya <adequateuser228@gmail.com>
2024-03-14 18:48:42 +0300

    add pages2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
     <div class = "nav-container">
         <a href="/" class ="nav-logo" title="Back to Homepages">Настя Палагина</a>
         <div class="nav-links">
-            {#each nav as link}
+            <!-- svelte-ignore missing-declaration -->
+            {#if link.isShow}
             <a href={link.href} class="link">{link.title}</a>
-            {/each}
+            {/if}
         </div>
     </div>
 </nav>
@@ -17,25 +18,30 @@
     const nav = [
         {
          title: "Обо мне",
-         href: "/about"
+         href: "/about",
+         isShow: true,
         },
         {
          title: "Блог",
          href: "/blog",
+         isShow: true,
         },
         {
          title: "Проект",
          href: "/project",
+         isShow: true,
         },
         {
          title: "Контакты",
-         href: "/contacts",
+         href: "/contact",
+         isShow: true,
         },
         {
          title: "temp",
-         href: "/temp"
-        }
-    ]
+         href: "/temp",
+         isShow: false,
+        },
+    ];
 </script>
 <style>
     .container{
