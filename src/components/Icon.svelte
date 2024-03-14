a691bc1d5999f47e01696e1f20a11c5c2ac5e17f
nastya <adequateuser228@gmail.com>
2024-03-14 20:46:27 +0300

    add todo app

---
+++ src/components/Icon.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let name;
+    const icons = [
+        {
+            name: "check-mark",
+        },
+        {
+            name: "delete",
+        },
+    ];
+    let icon = icons.find((item) => item.name === name);
+</script>
+    
--- src/routes/project/apps/1/+page.svelte
+++ src/routes/project/apps/1/+page.svelte
@@ -1,28 +1,55 @@
 <script>
- let newItem ='';
-    let todoList=[];
-    function add(){
-        if (newItem !=='') {
-            todoList=[
-                ...todoList,
-                {
-                    task: newItem,
-                    completed: false,
-                },
-            ];
-            newItem ='';
+
+    import Icon from '../../../../components/Icon.svelte';
+    let newItem = "";
+    let todoList = [];
+    function add() {
+        if (newItem !== "") {
+        todoList = [
+            ...todoList,
+            {
+              задача: newItem,
+              завершено: false,
+            },
+        ];
+        newItem = "";
         }
     }
-</script>
-<main>
-    <form on:submit|preventDefault={add}>
-    <input bind:value={newItem} placeholder="Enter to-do" />
-    <button class="add_to-do" on:click={add}><span>+</span>
-    </button>
-</form>
-</main>
-<style>
- main{
+    
+    function remove(index) {
+	todoList.splice(index, 1);
+	    todoList = todoList;
+    }
+
+    function complete(index) {
+	    todoList[index].completed = !todoList[index].completed;
+    }
+    </script>
+    <main>
+    <h1>My to-do list</h1>
+        <form on:submit|preventDefault={add}>
+            <input bind:value={newItem} placeholder="Enter to-do" />
+            <button class="add_to-do" on:click={add}><span>+</span></button>
+        </form>
+    
+    <div class="todos">
+        {#each todoList as item, index}
+            <div class="todo" class:completed={item.completed}>
+                <span class="todo__text">{item.task}</span>
+                <div class="todo__buttons">
+                    <button class="complete" on:click={()=> complete(index)}>
+                        <Icon name="check-mark" />
+                    </button>
+                    <button class="delete" on:click={()=> remove(index)}>
+                        <Icon name="delete" />
+                    </button>
+                </div>
+            </div>
+        {/each}
+    </div>
+    </main>
+    <style>
+    main {
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -31,16 +58,16 @@
         box-sizing: border-box;
         background: antiquewhite;
     }
-
-    form{
+    
+    form {
         width: 100%;
         max-width: 500px;
         display: flex;
         align-items: center;
         margin-bottom: 1rem;
     }
-
-    input{
+    
+    input {
         flex-grow: 1;
         width: 0;
         border: none;
@@ -51,4 +78,82 @@
         margin: 0;
         outline: none;
     }
-</style>
+    
+    .todo {
+        display: flex;
+        padding: 20px;
+        border-radius: 20px;
+        box-shadow: 0 0 15px rgb(0 0 0 / 20%);
+        background-color: hsla(0, 0%, 100%, 0.2);
+        margin-top: 1rem;
+        font-size: 1.2rem;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+    .todo__buttons {
+        display: flex;
+        align-items: center;
+        margin-left: 1rem;
+    }
+    
+    .todo button {
+        width: 32px;
+        height: 32px;
+        padding: 4px;
+        margin: 0;
+        flex-shrink: 0;
+    }
+    
+    h1 {
+        text-align: center;
+        font-size: 1.5rem;
+        margin: 2em 0;
+    }
+    
+    button.complete {
+        background-color: green;
+        border-radius: 100%;
+        cursor: pointer;
+    }
+
+    button.delete {
+        background-color: red;
+        border-radius: 100%;
+        cursor: pointer;
+    }
+    
+    button.delete:hover {
+        color: brown;
+        transition: color 100ms ease-out;
+    }
+    
+    button.complete:hover {
+        color: cabetblue;
+        transition: color 100ms ease-out;
+    }
+
+    .todo.completed {
+        color: slategray;
+    }
+    
+    .todo__text {
+        text-decoration: line-through;
+    }
+    
+    .todo.completed button {
+        color: silver;
+    }
+    
+    .todos {
+        width: 100%;
+        max-width: 500px;
+    }
+
+    .add_to-do {
+        background-color: transparent;
+        border: 1px solid black;
+        cursor: pointer;
+    }
+    </style>
+    
